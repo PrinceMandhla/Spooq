b2a906529d92198df6f493ee29bf9320a71349a5
PrinceMandhla <princemanhla@gmail.com>
2024-07-12 09:07:41 +0200

    all
    commit all

---
+++ spookmain/script.js
@@ -0,0 +1,15 @@
+/*
+prdcts=document.getElementById('pbtn');
+
+function Ppage()
+    {
+        let kards = document.getElementsByClassName('card');
+        for (let i =0;i<kards.length;i++)
+            {
+                kards[i].style.backgroundColor = 'red';
+            }
+
+    }
+
+prdcts.addEventListener('click',Ppage);
+*/
